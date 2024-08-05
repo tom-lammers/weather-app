3a6c82079b44a514bb8bece564d47e1c883dad21
Tom Lammers <tom.lammers@adesso.de>
2024-08-05 08:17:26 +0200

    Zipcode speichern

--- src/app/zipcode/zipcode.component.ts
+++ src/app/zipcode/zipcode.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import {UntypedFormControl, UntypedFormGroup} from "@angular/forms";
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, NonNullableFormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-zipcode',
   templateUrl: './zipcode.component.html',
 })
-export class ZipcodeComponent implements OnInit {
+export class ZipcodeComponent {
+  form: FormGroup<{ zipcode: FormControl<string> }>;
+  locations: string[] = [];
 
-  form: UntypedFormGroup<{zipcode: UntypedFormControl<string>}>;
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: NonNullableFormBuilder) {
+    this.form = this.fb.group({ zipcode: this.fb.control<string>('') });
   }
 
+  addLocation() {
+    this.locations.push(this.form.controls.zipcode.value);
+  }
 }
